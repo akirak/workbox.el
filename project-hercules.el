ba85d3c5ce46d28bffd1e9714b3c45c89bd9a260
Akira Komamura <akira.komamura@gmail.com>
2022-02-15 17:34:41 +0900

    Organize the code in project-hercules.el

--- project-hercules.el
+++ project-hercules.el
@@ -15,53 +15,7 @@
 
 (defvar project-hercules-commands nil)
 
-(defun project-hercules--ensure ()
-  (unless project-hercules-commands
-    (setq project-hercules-commands
-          (make-hash-table :test #'equal))))
-
-(defun project-hercules--normalize-root (root)
-  (file-truename (file-name-as-directory root)))
-
-(defun project-hercules--find-by-root (root)
-  (project-hercules--ensure)
-  (or (gethash (project-hercules--normalize-root root)
-               project-hercules-commands)
-      (when-let (worktrees (let ((default-directory root))
-                             (thread-last (magit-list-worktrees)
-                               (mapcar #'car)
-                               (delq root))))
-        (catch 'found
-          (dolist (worktree worktrees)
-            (when-let (command (gethash (project-hercules--normalize-root worktree)
-                                        project-hercules-commands))
-              (throw 'found command)))))))
-
-(defun project-hercules--remove-plist (plist prop)
-  (cond
-   ((null plist) plist)
-   ((eq prop (car plist))
-    (project-hercules--remove-plist (cddr plist) prop))
-   (t
-    `(,(nth 0 plist)
-      ,(nth 1 plist)
-      ,@(project-hercules--remove-plist (cddr plist) prop)))))
-
-(defun project-hercules--make-default-map (root)
-  "Create the default keymap for ROOT."
-  (let ((matches (seq-filter (lambda (rule)
-                               (project-hercules--test-rule (car rule)
-                                                            root))
-                             project-hercules-composed-maps)))
-    (make-composed-keymap
-     (seq-map (pcase-lambda (`(,_ ,map-symbol . ,_))
-                (symbol-value map-symbol))
-              matches))))
-
-(defun project-hercules--test-rule (condition root)
-  "Test the CONDITION of a rule against ROOT."
-  (cl-etypecase condition
-    (string (file-exists-p (expand-file-name condition root)))))
+;;;; Primary API
 
 (cl-defmacro project-hercules-make-map (root &rest hercules-args
                                              &key init &allow-other-keys)
@@ -85,14 +39,6 @@
           ,@hercules-args)
          (symbol-value map-symbol)))))
 
-(defun project-hercules-remove-project (root)
-  "Remove the definition for the project ROOT."
-  (interactive (list (or (project-root (project-current))
-                         (user-error "No project found"))))
-  (project-hercules--ensure)
-  (remhash (project-hercules--normalize-root root)
-           project-hercules-commands))
-
 (defun project-hercules-dispatch ()
   (interactive)
   (if-let (root (project-root (project-current)))
@@ -105,11 +51,33 @@
           (user-error "Not found for project %s" root)))
     (user-error "No project found")))
 
-(defun project-hercules--get-map (root)
-  "Return a list of keymaps for ROOT, if any."
-  (when-let* ((command (project-hercules--find-by-root root))
-              (symbol (get command 'project-hercules-map)))
-    (accessible-keymaps (symbol-value symbol))))
+;;;; The default keymaps
+
+(defun project-hercules--make-default-map (root)
+  "Create the default keymap for ROOT."
+  (let ((matches (seq-filter (lambda (rule)
+                               (project-hercules--test-rule (car rule)
+                                                            root))
+                             project-hercules-composed-maps)))
+    (make-composed-keymap
+     (seq-map (pcase-lambda (`(,_ ,map-symbol . ,_))
+                (symbol-value map-symbol))
+              matches))))
+
+(defun project-hercules--test-rule (condition root)
+  "Test the CONDITION of a rule against ROOT."
+  (cl-etypecase condition
+    (string (file-exists-p (expand-file-name condition root)))))
+
+;;;; Administration commands
+
+(defun project-hercules-remove-project (root)
+  "Remove the definition for the project ROOT."
+  (interactive (list (or (project-root (project-current))
+                         (user-error "No project found"))))
+  (project-hercules--ensure)
+  (remhash (project-hercules--normalize-root root)
+           project-hercules-commands))
 
 (defun project-hercules-display-keymap (root)
   "Display the keymap for the current project."
@@ -119,6 +87,50 @@
                              (user-error "No definition for root %s"
                                          root))
                          "*Hercules Keymap*"))
+
+(defun project-hercules--get-map (root)
+  "Return a list of keymaps for ROOT, if any."
+  (when-let* ((command (project-hercules--find-by-root root))
+              (symbol (get command 'project-hercules-map)))
+    (accessible-keymaps (symbol-value symbol))))
+
+;;;; Utilities
+
+;;;;; Hashtable operations
+
+(defun project-hercules--ensure ()
+  (unless project-hercules-commands
+    (setq project-hercules-commands
+          (make-hash-table :test #'equal))))
+
+(defun project-hercules--find-by-root (root)
+  (project-hercules--ensure)
+  (or (gethash (project-hercules--normalize-root root)
+               project-hercules-commands)
+      (when-let (worktrees (let ((default-directory root))
+                             (thread-last (magit-list-worktrees)
+                               (mapcar #'car)
+                               (delq root))))
+        (catch 'found
+          (dolist (worktree worktrees)
+            (when-let (command (gethash (project-hercules--normalize-root worktree)
+                                        project-hercules-commands))
+              (throw 'found command)))))))
+
+;;;;; Miscellaneous
+
+(defun project-hercules--normalize-root (root)
+  (file-truename (file-name-as-directory root)))
+
+(defun project-hercules--remove-plist (plist prop)
+  (cond
+   ((null plist) plist)
+   ((eq prop (car plist))
+    (project-hercules--remove-plist (cddr plist) prop))
+   (t
+    `(,(nth 0 plist)
+      ,(nth 1 plist)
+      ,@(project-hercules--remove-plist (cddr plist) prop)))))
 
 (provide 'project-hercules)
 ;;; project-hercules.el ends here
