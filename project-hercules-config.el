fc380bc1137e9dc82e22d1f79f97593697c29484
Akira Komamura <akira.komamura@gmail.com>
2022-02-16 09:57:57 +0900

    Add more default keybindings to the parent map

--- project-hercules-config.el
+++ project-hercules-config.el
@@ -2,14 +2,30 @@
 
 (require 'project-hercules)
 
-(defvar project-hercules-nix-map
+;; TODO: Add declare-function forms
+
+;;;; Add entries to the parent map
+
+(let ((parent project-hercules-parent-map))
+  (define-key parent "c" #'project-compile)
+  (define-key parent "l" #'magit-log)
+  (define-key parent "s" #'magit-status))
+
+(dolist (fn '(project-compile
+              magit-log
+              magit-status))
+  (add-to-list 'project-hercules-hide-funs fn))
+
+;;;; Composition examples
+
+(defvar project-hercules-nix-flake-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "f" #'nix-flake-dispatch)
+    (define-key map "n" #'nix-flake-dispatch)
     map))
 
 (setq project-hercules-composed-maps
       '(("flake.nix"
-         project-hercules-nix-map)))
+         project-hercules-nix-flake-map)))
 
 (provide 'project-hercules-config)
 ;;; project-hercules-config.el ends here
