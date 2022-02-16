0790839f311435cc812df827638158dae0a4f2ae
Akira Komamura <akira.komamura@gmail.com>
2022-02-16 17:32:00 +0900

    feat(config): Add a separate map for magit

--- project-hercules-config.el
+++ project-hercules-config.el
@@ -7,16 +7,13 @@
 (declare-function magit-log "ext:magit")
 (declare-function magit-stage-modified "ext:magit")
 (declare-function magit-status "ext:magit")
+(declare-function magit-push "ext:magit")
 (declare-function nix-flake-dispatch "ext:nix-flake")
 
 ;;;; Add entries to the parent map
 
 (let ((parent project-hercules-parent-map))
-  (define-key parent "b" #'project-compile)
-  (define-key parent "c" #'magit-commit)
-  (define-key parent "l" #'magit-log)
-  (define-key parent "m" #'magit-stage-modified)
-  (define-key parent "s" #'magit-status))
+  (define-key parent "b" #'project-compile))
 
 (dolist (symbol '(project-compile
                   magit-log))
@@ -29,9 +26,20 @@
     (define-key map "n" #'nix-flake-dispatch)
     map))
 
+(defvar project-hercules-magit-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "c" #'magit-commit)
+    (define-key map "l" #'magit-log)
+    (define-key map "m" #'magit-stage-modified)
+    (define-key map "s" #'magit-status)
+    (define-key map "P" #'magit-push)
+    map))
+
 (setq project-hercules-composed-maps
       '(("flake.nix"
-         project-hercules-nix-flake-map)))
+         project-hercules-nix-flake-map)
+        (".git"
+         project-hercules-magit-map)))
 
 (provide 'project-hercules-config)
 ;;; project-hercules-config.el ends here
