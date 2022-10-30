73b6954a3c8afadcc935f1745c8ae8c0a9462f34
Akira Komamura <akira.komamura@gmail.com>
2022-10-30 12:53:07 +0900

    build: Regenerate the package configuration

---
+++ .nomake/flake.nix
@@ -0,0 +1,25 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    hercules = {
+      flake = false;
+      owner = "cyruseuros";
+      repo = "hercules";
+      type = "github";
+    };
+    which-key = {
+      flake = false;
+      owner = "justbur";
+      repo = "emacs-which-key";
+      type = "github";
+    };
+    workbox = {
+      flake = false;
+      owner = "akirak";
+      repo = "workbox.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
