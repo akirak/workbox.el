0447c82256494faf50ce3c8e3fa1ee12052d1d7e
Akira Komamura <akira.komamura@gmail.com>
2022-10-30 14:03:39 +0900

    Add workbox-transient

---
+++ workbox-transient.el
@@ -0,0 +1,32 @@
+;;; workbox-transient.el --- Transient interface -*- lexical-binding: t -*-
+
+;;;###autoload (autoload 'workbox-transient "workbox-transient" nil 'interactive)
+(transient-define-prefix workbox-transient ()
+  "A convenient transient command for your projects."
+  [:description
+   workbox-transient-describe-package
+   :if (lambda () workbox-package-config)
+   ("p" "Run a package command" workbox-run-some-package-manager)]
+  ["Compile"
+   ("r" workbox-transient-recompile)
+   ("c" "Compile at the project root" project-compile)]
+  (interactive)
+  (workbox-locate-package)
+  (transient-setup 'workbox-transient))
+
+(defun workbox-transient-describe-package ()
+  (when workbox-package-config
+    (format "Package: %s in %s"
+            (car workbox-package-config)
+            (abbreviate-file-name workbox-default-directory))))
+
+(transient-define-suffix workbox-transient-recompile ()
+  :description (lambda () (format "Recompile (%s in %s)"
+                                  compile-command
+                                  (abbreviate-file-name compilation-directory)))
+  :if (lambda () compile-command)
+  (interactive)
+  (recompile))
+
+(provide 'workbox-transient)
+;;; workbox-transient.el ends here
